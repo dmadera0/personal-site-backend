2eb471060b65c08efc2b8a215c94cf5532f6f132
Daniel <dmadera0@gmail.com>
2020-12-29 15:01:44 -0700

    first commit

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
 const express = require('express');
-const express = require('express');
-const express = require('express');
-const express = require('express'); 
+
+const bodyParser = require('bodyParser');
+
+const cors = require('cors');
+
+const sendGrid = require('@sendgrid/mail');
+
+const app = express();
+
+app.use(bodyParser.json())
+
+app.use(cors());
+
+app.use(cors());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Change later to only allow our server
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
